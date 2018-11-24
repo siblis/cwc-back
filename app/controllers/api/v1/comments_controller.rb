c31312e8244de753bed0020f910f7a68a4517999
Vadim Petrov <pvg.tag@yandex.ru>
2018-11-24 21:12:01 +0300

    fix comment_controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -18,7 +18,7 @@ module Api::V1
       @comment = Comment.new(comment_params)
 
       if @comment.save
-        render json: @comment, status: :created, location: @comment
+        render json: @comment, status: :created
       else
         render json: @comment.errors, status: :unprocessable_entity
       end
