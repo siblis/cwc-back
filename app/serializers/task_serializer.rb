8a7d74b6d73cdc654ba1bfa24007767c57eea859
Vadim Petrov <pvg.tag@yandex.ru>
2018-12-01 23:11:33 +0300

    Fix TaskUser association

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,5 +1,5 @@
 class TaskSerializer < ActiveModel::Serializer
   attributes :id, :title, :body, :deadline, :completed, :started, :project_id, :created_at, :updated_at
   belongs_to :user
-  belongs_to :doer, class_name: "User", foreign_key: "doer_id"
+  belongs_to :doer, class_name: "User"
 end
