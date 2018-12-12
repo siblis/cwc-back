5fc9a826d3d0fbd7aa6a2bd3f3e9fdc031e4f246
Vadim Petrov <pvg.tag@yandex.ru>
2018-12-12 20:48:02 +0300

    Add user_mailer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -17,6 +17,7 @@ module Api::V1
     def create
       @user = User.new(user_params)
       if @user.save
+        UserMailer.welcome_email(@user).deliver_now
         render json: @user, status: :created
       else
         render json: @user.errors, status: :unprocessable_entity
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,9 +31,26 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :sendmail
+# Defaults to:
+# config.action_mailer.sendmail_settings = {
+#   location: '/usr/sbin/sendmail',
+#   arguments: '-i'
+# }
+  config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = true
+  config.action_mailer.default_options = {from: 'cwc.back@gmail.com'}
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'cwc.com',
+    user_name:            'cwc.back',
+    password:             '!QAZxsw2',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
